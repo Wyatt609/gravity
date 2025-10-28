89b2228821098a4d9d472b91ff956d2d2dd01244
Wyatt609 <wyattfoster609@icloud.com>
2025-10-27 22:20:19 -0500

    end
    fixed end trigger

--- Assets/Scripts/LevelEndTrigger.cs
+++ Assets/Scripts/LevelEndTrigger.cs
@@ -4,10 +4,30 @@ using UnityEngine.SceneManagement;
 public class LevelEndTrigger : MonoBehaviour
 {
     public string nextScene;
+    public bool useNextBuildIndex = true;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-            SceneManager.LoadScene(nextScene);
+        if (other.GetComponent<CharacterController>() == null && !other.CompareTag("Player")) return;
+        LoadNext();
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<CharacterController>() == null && !other.CompareTag("Player")) return;
+        LoadNext();
+    }
+
+    void LoadNext()
+    {
+        if (useNextBuildIndex)
+        {
+            int i = SceneManager.GetActiveScene().buildIndex + 1;
+            if (i < SceneManager.sceneCountInBuildSettings) SceneManager.LoadScene(i);
+        }
+        else
+        {
+            if (!string.IsNullOrEmpty(nextScene)) SceneManager.LoadScene(nextScene);
+        }
     }
 }
